715632c098c7107a6a9a92359ef0f7b6be74bc2a
Michele Ceccacci <michelececcacci1@gmail.com>
2023-05-04 15:33:47 +0200

    start setting up bubbletea

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
-package main;
+package main
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+
+}
+
+func initialModel() model {
+	return model{}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return nil, nil
+}
+
+func (m model) View() string {
+	return ""
+}
+
 
 func main() {
+	p := tea.NewProgram(initialModel())
+	_, err := p.Run()
+	if err != nil {
+		panic(err)
+	}
 }
