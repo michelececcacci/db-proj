fe8c0d505f694e464ef43886b31fe7868c3fa0d3
Michele Ceccacci <michelececcacci1@gmail.com>
2023-05-17 17:07:56 +0200

    trying to make the image buildable

--- go.mod
+++ go.mod
@@ -2,12 +2,14 @@ module github.com/michelececcacci/db-proj
 
 go 1.18
 
-require github.com/charmbracelet/bubbletea v0.23.2
+require (
+	github.com/charmbracelet/bubbletea v0.23.2
+	github.com/lib/pq v1.10.9
+)
 
 require (
 	github.com/aymanbagabas/go-osc52 v1.2.1 // indirect
 	github.com/containerd/console v1.0.3 // indirect
-	github.com/kyleconroy/sqlc v1.18.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"database/sql"
+	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
+	_ "github.com/lib/pq"
 	"github.com/michelececcacci/db-proj/queries"
 )
 
@@ -29,13 +31,28 @@ func (m model) View() string {
 }
 
 
-func main() {
-	q := queries.Queries{}
-	r, _ := q.Something(context.Background())
-	fmt.Println(r)
+func run() error {
+	db, err := sql.Open("postgres", "user=postgres dbname=logica sslmode=verify-full")
+	if err != nil {
+		return err
+	}
+	qq := queries.New(db)
+	r, err := qq.Something(context.Background())
+	if err != nil {
+		return err
+	}
+	log.Println(r)
 	// p := tea.NewProgram(initialModel())
 	// _, err := p.Run()
 	// if err != nil {
 	// 	panic(err)
+	// }
+	return nil
+}
+
+func main() {
+	log.Println("hi")
+	// if err :=run(); err != nil {
+	// 	log.Fatal(err)
 	// }
 }
