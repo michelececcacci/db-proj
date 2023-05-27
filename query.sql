3557e76ea97bb4391c58b59dc405eed7ccd15c34
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-05-27 21:08:41 +0200

    Add insert queries for SEGUIRE and REGIONE and add random access query for UTENTE

--- query.sql
+++ query.sql
@@ -9,6 +9,14 @@ INSERT INTO UTENTE (Username, DataDiNascita, Nome, Cognome, Domicilio)
 INSERT INTO STORICO_PASSWORD (Username, Password, DataInserimento)
     VALUES ($1, $2, $3);
 
+-- name: InsertFollower :exec
+INSERT INTO SEGUIRE (UsernameSeguace, UsernameSeguito, DataInizio, DataFine)
+  VALUES ($1, $2, $3, $4);
+
+-- name: InsertRegion :exec
+INSERT INTO REGIONE (Nome, Superregione) 
+  VALUES ($1, $2);
+
 -- name: GetFollowers :many
 SELECT usernameseguace FROM SEGUIRE WHERE usernameseguito = $1 AND DataFine IS NULL;
 
@@ -26,3 +34,12 @@ FROM (
 
 -- name: GetPastPasswords :many
 SELECT Password FROM  STORICO_PASSWORD WHERE username = $1;
+
+-- random accesses
+
+-- name: GetRandomUser :one
+SELECT Username
+FROM UTENTE
+ORDER BY random()
+LIMIT 1;
+
